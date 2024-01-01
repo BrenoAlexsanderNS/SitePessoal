0cb28b7c768e855445e4dbf4c7b5e75aef7578c1
Your Name <you@example.com>
2024-01-01 17:36:15 -0300

    Teste

---
+++ script.js
@@ -0,0 +1,16 @@
+function toogleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  // Pegar a tag img
+  const img = document.querySelector("#profile img")
+
+  // Subistituir a imagem
+  if (html.classList.contains("light")) {
+  // Se tiver light mode, adicionar imagem light
+    img.setAttribute("src", "./assets/avatar-light.png")
+  } else {
+  // Se tiver sem light mode, manter a imagem normal
+    img.setAttribute("src", "./assets/avatar.png")
+  }
+}
